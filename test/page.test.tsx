1162756d2aa64bc1f1c87aad260d10442cdf0d44
Vaughn Walker <vaughn.walker@integral.io>
2024-05-06 16:46:30 -0400

    Add button and input field. Remove global styles

--- test/page.test.tsx
+++ test/page.test.tsx
@@ -1,10 +1,11 @@
 import {render, screen} from "@testing-library/react";
 import Home from "@/app/page";
 
-describe("Main Page", () => {
+describe("Home Page", () => {
     it("should render an input field", () => {
         render(<Home/>)
 
         expect(screen.getByRole("textbox")).toBeInTheDocument()
+        expect(screen.getByRole("button")).toHaveTextContent("Search")
     })
 })
